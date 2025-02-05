4d882e4cf87f832f818afd149a5d7c0ccaabab7f
Mawen Salignat-Moandal <salignatmoandal@MacBook-Pro-de-Mawen.local>
2025-02-05 10:15:45 +0100

    test

--- us-east-1/compute/backend.tf
+++ us-east-1/compute/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "tf-state-mawen"
     encrypt = true
-    region  = "us-east-1"
+    region  = "us-east-2"
     key     = "tf-acme-infra/us-east-1/compute.tfstate"
   }
 }
